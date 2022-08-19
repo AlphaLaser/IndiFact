1f621c1a5f67d360f5a237ba459a95b90db12e36
Adit <aditmagotra@gmail.com>
2022-08-19 11:44:57 +0530

    ➕  split.py to split Data for model training

--- Split Data/split.py
+++ Split Data/split.py
@@ -8,7 +8,7 @@ X = np.array(data['text'])
 y = np.array(data['numeric_labels'])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-
+ 
 np.save("Data/train_test_split/X_train.npy", X_train, allow_pickle=True)
 np.save("Data/train_test_split/X_test.npy", X_test, allow_pickle=True)
 np.save("Data/train_test_split/y_train.npy", y_train, allow_pickle=True)
